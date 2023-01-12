b3b2589719f1cd08c75d930c35e5b8787afdd83d
Ondar222 <Arslanondar2003@gmail.com>
2023-01-12 10:29:08 +0700

    my-commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheNavigation />
+ <div class="container">
+  <NuxtPage />
+ </div>
+  </div>
+</template>
+
+<style>
+.container {
+  margin-top: 60px auto 0;
+  padding: 1rem;
+  max-width: 960px;
+}
+</style>
---
+++ components/TheBody.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  render () {
+  return <h1>This is Body</h1>
+  }
+})
