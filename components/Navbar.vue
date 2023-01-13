f031034e772297fab3eb60faed4de89e2fed6adc
Ondar222 <Arslanondar2003@gmail.com>
2023-01-13 17:28:35 +0700

    cpm system commit

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,43 +1,57 @@
 <template>
   <header>
-    <div class="flex justify-betweens align-items-center bg-gradient-to-r from-blue-400 to to-purple-400 h-20 text-white">
+    <div class="bg-gradient-to-r from-blue-400 to to-purple-400 w-1/4 h-screen text-black">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="text-2xl" href="#"><b>CPM Система</b></a>
-        <div class="flex justify-between align-items-center text-white">
-          <ul class="flex justify-between mx-auto space-x-10 text-xl ml-5 font-bold">
+        <div class=" text-black">
+          <ul class="mx-auto space-x-10 text-2xl ml-5 font-bold">
               <li class="nav-item">
                 <nuxt-link exact no-prefetch active-class="active" class="nav-link" to="/">
-                  Категории
+                  <a class="text-4xl" href=""><h1>CPM система</h1></a>
+                </nuxt-link>
+              </li>
+              <li class="nav-item">
+                <nuxt-link exact no-prefetch active-class="active" class="nav-link" to="/">
+                  Складский учёт
                 </nuxt-link>
               </li>
               <li class="nav-item">
                 <nuxt-link active-class="active" class="nav-link" to="/about">
-                  Новая запись
+                  Маркетинг
                 </nuxt-link>
               </li>
               <li class="nav-item">
                 <nuxt-link active-class="active" class="nav-link" to="/history">
-                  История
+                  Сайты и Магазины
+                </nuxt-link>
+              </li>
+              <li class="nav-item">
+                <nuxt-link active-class="active" class="nav-link" to="/contacts">
+                  Задачи и Проекты
+                </nuxt-link>
+              </li>
+              <li class="nav-item">
+                <nuxt-link active-class="active" class="nav-link" to="/company">
+                  Совместная работа
                 </nuxt-link>
               </li>
               <li class="nav-item">
                 <nuxt-link active-class="active" class="nav-link" to="/sales">
-                  Счет
+                  Компания
                 </nuxt-link>
               </li>
               <li class="nav-item">
-                <nuxt-link active-class="active" class="nav-link" to="/accounting">
-                  Домашняя бухгалтерия
+                <nuxt-link active-class="active" class="nav-link" to="/acoounting">
+                  Автоматизцаия
                 </nuxt-link>
               </li>
               <li class="nav-item" v-if="!hasToken">
                 <nuxt-link active-class="active" class="nav-link" to="/plan">
-                  Планирование
+                  Приложения
                 </nuxt-link>
               </li>
               <li class="nav-item" v-if="!hasToken">
                 <nuxt-link active-class="active" class="nav-link" to="/profile">
-                  Профиль
+                  Еще
                 </nuxt-link>
               </li>
               <li class="nav-item" v-else>
@@ -51,6 +65,38 @@
     </div>
       </header>
 </template>
+
+<script>
+export default {
+  computed: {
+    hasToken() {
+      return this.$store.getters.hasToken
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<script>
+export default {
+  computed: {
+    hasToken() {
+      return this.$store.getters.hasToken
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
 
 <script>
 export default {
--- pages/history.vue
+++ pages/history.vue
@@ -1,8 +1,10 @@
 <template>
   <section>
-    <h1>Error 404</h1>
-
-    <nuxt-link to="/">История записей</nuxt-link>
+    <div class="flex space-x-5">
+      <h1 class="font-bold text-2xl m-2 text-red-500">Маркетинг</h1>
+      <input type="text" class="w-1-2 h-10 bg-gray-100 pl-5 p-2 m-2 focus:outline-nine rounded-full" placeholder="Фильтр + Поиск">
+      <a class="bg-blue-400 text-white m-2 font-bold p-2 text-sm" href="#">Добавить контакт</a>
+    </div>
   </section>
 </template>
 
